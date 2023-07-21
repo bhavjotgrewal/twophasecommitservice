ffdf9d6c53ef7bbc10fe0a89ba8170a71d5fe4fe
Bhavjot Grewal <bhavjot.grewal@mail.utoronto.ca>
2023-07-21 14:05:00 -0500

    Added unit tests, implemented TransactionManager

--- TwoPhaseCommitService/internal/tm/tm.go
+++ TwoPhaseCommitService/internal/tm/tm.go
@@ -3,22 +3,66 @@ package tm
 import (
 	pb "TwoPhaseCommitService/TwoPhaseCommitService/pkg/twophasecommit"
 	"context"
+	"sync"
 )
+
+type Participant interface {
+	Prepare(txnID string) bool
+	Commit(txnID string) bool
+}
 
 type transactionManager struct {
 	pb.UnimplementedTwoPhaseCommitServer
+	participants []Participant
+	mux          sync.Mutex
+	prepared     map[string]bool
 }
 
-func NewTransactionManager() *transactionManager {
-	return &transactionManager{}
+func NewTransactionManager(participants []Participant) *transactionManager {
+	return &transactionManager{
+		participants: participants,
+		prepared:     make(map[string]bool),
+	}
 }
 
 func (tm *transactionManager) Prepare(ctx context.Context, in *pb.PrepareRequest) (*pb.VoteResponse, error) {
-	// TODO: Implement prepare phase
-	return &pb.VoteResponse{}, nil
+	txnID := in.GetTransactionId()
+	vote := pb.VoteResponse_YES
+	tm.mux.Lock()
+	defer tm.mux.Unlock()
+
+	for _, participant := range tm.participants {
+		if !participant.Prepare(txnID) {
+			// If any participant votes NO, mark the transaction as not prepared and return immediately
+			tm.prepared[txnID] = false
+			return &pb.VoteResponse{Vote: pb.VoteResponse_NO}, nil
+		}
+	}
+
+	tm.prepared[txnID] = true
+	return &pb.VoteResponse{Vote: vote}, nil
 }
 
 func (tm *transactionManager) Commit(ctx context.Context, in *pb.CommitRequest) (*pb.AckResponse, error) {
-	// TODO: Implement commit phase
-	return &pb.AckResponse{}, nil
+	txnID := in.GetTransactionId()
+	success := false
+	tm.mux.Lock()
+	defer tm.mux.Unlock()
+
+	if tm.prepared[txnID] {
+		success = true
+		for _, participant := range tm.participants {
+			if !participant.Commit(txnID) {
+				success = false
+				break
+			}
+		}
+	}
+
+	// If any participant's commit failed, mark the transaction as not prepared
+	if !success {
+		tm.prepared[txnID] = false
+	}
+
+	return &pb.AckResponse{Success: success}, nil
 }
--- TwoPhaseCommitService/internal/tm/tm_test.go
+++ TwoPhaseCommitService/internal/tm/tm_test.go
@@ -1,13 +1,114 @@
 package tm
 
 import (
+	"TwoPhaseCommitService/TwoPhaseCommitService/mocks"
+	pb "TwoPhaseCommitService/TwoPhaseCommitService/pkg/twophasecommit"
+	"context"
 	"testing"
 )
 
-func TestPrepare(t *testing.T) {
-	// TODO: Implement tests for prepare phase
+func TestTransactionManager_Prepare(t *testing.T) {
+	participants := []Participant{
+		&mocks.MockParticipant{Name: "participant1", PrepareSuccess: true},
+		&mocks.MockParticipant{Name: "participant2", PrepareSuccess: true},
+		&mocks.MockParticipant{Name: "participant3", PrepareSuccess: true},
+	}
+
+	tm := NewTransactionManager(participants)
+
+	// Prepare the transaction
+	res, err := tm.Prepare(context.Background(), &pb.PrepareRequest{TransactionId: "123"})
+	if err != nil {
+		t.Fatalf("unexpected error from Prepare: %v", err)
+	}
+
+	if res.Vote != pb.VoteResponse_YES {
+		t.Errorf("expected vote YES, got %s", res.Vote)
+	}
+
+	// Check that all participants have been prepared
+	for _, participant := range participants {
+		if p := participant.(*mocks.MockParticipant); !p.Prepared {
+			t.Errorf("expected participant %s to be prepared", p.Name)
+		}
+	}
 }
 
-func TestCommit(t *testing.T) {
-	// TODO: Implement tests for commit phase
+func TestTransactionManager_Commit(t *testing.T) {
+	participants := []Participant{
+		&mocks.MockParticipant{Name: "participant1", PrepareSuccess: true, CommitSuccess: true},
+		&mocks.MockParticipant{Name: "participant2", PrepareSuccess: true, CommitSuccess: true},
+		&mocks.MockParticipant{Name: "participant3", PrepareSuccess: true, CommitSuccess: true},
+	}
+
+	tm := NewTransactionManager(participants)
+
+	// Prepare the transaction
+	_, err := tm.Prepare(context.Background(), &pb.PrepareRequest{TransactionId: "123"})
+	if err != nil {
+		t.Fatalf("unexpected error from Prepare: %v", err)
+	}
+
+	// Commit the transaction
+	res, err := tm.Commit(context.Background(), &pb.CommitRequest{TransactionId: "123"})
+	if err != nil {
+		t.Fatalf("unexpected error from Commit: %v", err)
+	}
+
+	if !res.Success {
+		t.Errorf("expected commit to be successful")
+	}
+
+	// Check that all participants have been committed
+	for _, participant := range participants {
+		if p := participant.(*mocks.MockParticipant); !p.Committed {
+			t.Errorf("expected participant %s to be committed", p.Name)
+		}
+	}
+}
+
+func TestTransactionManager_PrepareFails(t *testing.T) {
+	participants := []Participant{
+		&mocks.MockParticipant{Name: "participant1", PrepareSuccess: true},
+		&mocks.MockParticipant{Name: "participant2", PrepareSuccess: false},
+		&mocks.MockParticipant{Name: "participant3", PrepareSuccess: true},
+	}
+
+	tm := NewTransactionManager(participants)
+
+	// Prepare the transaction
+	res, err := tm.Prepare(context.Background(), &pb.PrepareRequest{TransactionId: "123"})
+	if err != nil {
+		t.Fatalf("unexpected error from Prepare: %v", err)
+	}
+
+	if res.Vote != pb.VoteResponse_NO {
+		t.Errorf("expected vote NO, got %s", res.Vote)
+	}
+}
+
+func TestTransactionManager_CommitFails(t *testing.T) {
+	participants := []Participant{
+		&mocks.MockParticipant{Name: "participant1", PrepareSuccess: true, CommitSuccess: true},
+		&mocks.MockParticipant{Name: "participant2", PrepareSuccess: true, CommitSuccess: false},
+		&mocks.MockParticipant{Name: "participant3", PrepareSuccess: true, CommitSuccess: true},
+	}
+
+	tm := NewTransactionManager(participants)
+
+	// Prepare the transaction
+	_, err := tm.Prepare(context.Background(), &pb.PrepareRequest{TransactionId: "123"})
+	if err != nil {
+		t.Fatalf("unexpected error from Prepare: %v", err)
+	}
+
+	// Commit the transaction
+	res, err := tm.Commit(context.Background(), &pb.CommitRequest{TransactionId: "123"})
+	if err != nil {
+		t.Fatalf("unexpected error from Commit: %v", err)
+	}
+
+	if res.Success {
+		t.Errorf("expected commit to fail")
+	}
 }
